c455a6fe2521f084754f2bf69c47b04881243668
jessval <48558482+jessval@users.noreply.github.com>
2020-06-23 14:13:55 -0400

    Nested components and Es6

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+//stateless functional component
+
+//always return JSX ( a syntax in react similar to html)
+//JSX RULES:
+//return single element
+//div / section /article
+//use camelCase for html attribute
+//className instead of class
+//close every element
+//formating
+
+//function name has to be captilized
 function Greeting() {
-  return <h1>Hi I'm Jessica and this my first react component!</h1>;
+  return (
+    <>
+      <section>
+        <Person />
+        <Message />
+      </section>
+    </>
+  );
 }
+const Person = () => <h2>Hello World</h2>;
+const Message = () => <p>It's me Jessica.</p>;
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
