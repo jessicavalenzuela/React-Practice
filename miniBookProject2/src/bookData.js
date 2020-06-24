c289ebc36d6a83ceddffb225178ad7a3d3b20e0d
jessval <48558482+jessval@users.noreply.github.com>
2020-06-24 16:42:46 -0400

    Mini book project 2

---
+++ miniBookProject2/src/bookData.js
@@ -0,0 +1,25 @@
+const books = [
+  {
+    id: 1,
+    img:
+      "https://s26162.pcdn.co/wp-content/uploads/2019/07/used-books-store-2.jpg",
+    title: "book 1",
+    author: "author 1",
+  },
+  {
+    id: 2,
+    img:
+      "https://s26162.pcdn.co/wp-content/uploads/2019/07/used-books-store-2.jpg",
+    title: "book 2",
+    author: "author 2",
+  },
+  {
+    id: 3,
+    img:
+      "https://s26162.pcdn.co/wp-content/uploads/2019/07/used-books-store-2.jpg",
+    title: "book 3",
+    author: "author 3",
+  },
+];
+
+export default books;
