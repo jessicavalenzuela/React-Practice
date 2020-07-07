00c9aaa694e5a383098ffc329d778c2fdcbce8c0
jessval <48558482+jessval@users.noreply.github.com>
2020-07-07 11:42:13 -0400

    Adding form to todo-list project

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
-import uuid from "uuid";
+import { v1 as uuid } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInputs";
 import TodoList from "./components/TodoList";
 import TodoItem from "./components/TodoInputs";
-
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "wake up!" },
+      { id: 2, title: "brush teeth!" },
+    ],
+    id: uuid(),
+    item: "",
+    editITem: false,
+  };
+  handleChange = (e) => {};
+  handleSubmit = (e) => {};
+  clearList = () => {};
+  handleDelete = (id) => {};
+  handleEdit = (id) => {};
+
   render() {
     return (
       <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center"> Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editITem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
         </div>
       </div>
     );
