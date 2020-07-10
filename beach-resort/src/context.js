74f9e9ed67cdc83b14454b068bc76675911f9b66
jessval <48558482+jessval@users.noreply.github.com>
2020-07-10 16:35:45 -0400

    Room filters

--- beach-resort/src/context.js
+++ beach-resort/src/context.js
@@ -9,6 +9,15 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   //Getting Data
@@ -17,11 +26,16 @@ class RoomProvider extends Component {
     //this.getData
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((items) => items.price));
+    let maxSize = Math.max(...rooms.map((items) => items.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
   formatData(items) {
@@ -39,10 +53,60 @@ class RoomProvider extends Component {
     const room = tempRooms.find((room) => room.slug === slug);
     return room;
   };
+  handleChange = (event) => {
+    const target = event.target;
+    const value = event.type === "checkbox" ? target.checked : target.value;
+    const name = event.target.name;
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterRooms
+    );
+  };
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      mixSize,
+      maxSize,
+      breakfast,
+      pets,
+    } = this.state;
+
+    //allrooms
+    let tempRooms = [...rooms];
+    //transform values
+    capacity = parseInt(capacity);
+
+    //filter type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+    //filter capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity);
+    }
+    //filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    //change state
+    this.setState({
+      sortedRooms: tempRooms,
+    });
+  };
 
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
@@ -50,5 +114,15 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {(value) => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
 
 export { RoomProvider, RoomConsumer, RoomContext };
--- beach-resort/src/pages/rooms.js
+++ beach-resort/src/pages/rooms.js
@@ -2,14 +2,19 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/banner";
 import { Link } from "react-router-dom";
+import RoomsContainer from "../components/roomsContainer";
+
 export default function rooms() {
   return (
-    <Hero hero="roomsHero">
-      <Banner title="Our Rooms">
-        <Link to="/" className="btn-primary">
-          Return Home
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="Our Rooms">
+          <Link to="/" className="btn-primary">
+            Return Home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer />
+    </>
   );
 }
