bbc5b1a2d319ff5ec7daf03ed4eb35127c9ff2de
jessval <48558482+jessval@users.noreply.github.com>
2020-07-13 10:37:14 -0400

    finishing functionality

--- beach-resort/src/context.js
+++ beach-resort/src/context.js
@@ -55,7 +55,7 @@ class RoomProvider extends Component {
   };
   handleChange = (event) => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
     this.setState(
       {
@@ -70,7 +70,7 @@ class RoomProvider extends Component {
       type,
       capacity,
       price,
-      mixSize,
+      minSize,
       maxSize,
       breakfast,
       pets,
@@ -91,6 +91,20 @@ class RoomProvider extends Component {
     }
     //filter by price
     tempRooms = tempRooms.filter((room) => room.price <= price);
+
+    //filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
 
     //change state
     this.setState({
