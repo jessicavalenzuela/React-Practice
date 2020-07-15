af3896ac8c26d3daea34a3b4b942c826bd5efade
jessval <48558482+jessval@users.noreply.github.com>
2020-07-15 12:54:49 -0400

    handleSubmit function. Adding new charge and amount + calculator functionality

--- budget-calculator/src/App.js
+++ budget-calculator/src/App.js
@@ -12,13 +12,44 @@ const initialExpenses = [
 ];
 
 function App() {
+  //state values
+
   const [expenses, setExpenses] = useState(initialExpenses);
+  //single expense
+  const [charge, setCharge] = useState("");
+  //single amount
+  const [amount, setAmount] = useState("");
+
+  //fuctionality
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = (e) => {
+    setAmount(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const singleExpense = { id: uuid(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      setCharge("");
+      setAmount("");
+    } else {
+      //alert
+    }
+  };
   return (
     <>
       <Alert />
       <h1>budget calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
@@ -26,7 +57,7 @@ function App() {
         <span className="total">
           $
           {expenses.reduce((acc, curr) => {
-            return (acc += curr.amount);
+            return (acc += parseInt(curr.amount));
           }, 0)}
         </span>
       </h1>
