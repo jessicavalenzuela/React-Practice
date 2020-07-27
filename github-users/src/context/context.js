59f23b334a6cc5d9537c137f66025e2f74108d4a
jessval <48558482+jessval@users.noreply.github.com>
2020-07-27 13:56:14 -0400

    Search component: search user and get user data functionality with error if user doesn't exist

--- github-users/src/context/context.js
+++ github-users/src/context/context.js
@@ -13,8 +13,53 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
+  //request loading
+  const [requests, setRequests] = useState(0);
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    //setLoading(true)
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setGithubUser(response.data);
+    } else {
+      toggleError(true, "there is no user with that username");
+    }
+  };
+  //check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          toggleError(true, "sorry you have exeeded your hourly rate limit!");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+  //error
+  useEffect(checkRequests, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
