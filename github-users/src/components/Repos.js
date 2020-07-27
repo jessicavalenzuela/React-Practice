187d4084dcb24654856b8c1361c8d19949f0f96a
jessval <48558482+jessval@users.noreply.github.com>
2020-07-27 12:38:57 -0400

    Most popular repos and most forked repos column + bar charts

--- github-users/src/components/Repos.js
+++ github-users/src/components/Repos.js
@@ -38,25 +38,30 @@ const Repos = () => {
     })
     .slice(0, 5);
 
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13",
+  //stars, forked
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, vlaue: forks };
+      return total;
     },
     {
-      label: "CSS",
-      value: "23",
-    },
-    {
-      label: "Javascript",
-      value: "80",
-    },
-  ];
+      stars: {},
+      forks: {},
+    }
+  );
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
         {/* <ExampleChart data={chartData} /> */}
       </Wrapper>
     </section>
