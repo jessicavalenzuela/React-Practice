5566eaaa035481a5a3ba49d68ee215f1d378d0eb
jessval <48558482+jessval@users.noreply.github.com>
2020-07-28 10:59:17 -0400

    authenticated users redirect to home page, logout redirect to login page

--- github-users/src/App.js
+++ github-users/src/App.js
@@ -4,21 +4,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" exact={true}>
-            <Dashboard></Dashboard>
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <AuthWrapper>
+      <div>
+        <Router>
+          <Switch>
+            <PrivateRoute path="/" exact={true}>
+              <Dashboard></Dashboard>
+            </PrivateRoute>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="*">
+              <Error />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </AuthWrapper>
   );
 }
 
