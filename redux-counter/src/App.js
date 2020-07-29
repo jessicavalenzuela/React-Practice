cf841ae1c0810bc5762c7b5fe63bfbca52588a47
jessval <48558482+jessval@users.noreply.github.com>
2020-07-29 13:25:59 -0400

    redux reduce actions

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
 import Counter from "./Counter";
+import { createStore } from "redux";
+//setup initial state
+const defaultState = {
+  count: 76,
+  name: "bob",
+};
+//setup reducer
+function reducer(state, action) {
+  // console.log({ state, action });
+  // if (action.type === "DECREASE") {
+  //   state.count = state.count - 1;
+  //   return ;
+  // }
+  switch (action.type) {
+    case "DECREASE":
+      return { ...state, count: state.count - 1 };
+    case "INCREASE":
+      return { ...state, count: state.count + 1 };
+    case "RESET":
+      return { ...state, count: 0 };
+  }
+
+  return state;
+}
+//setup store
+const store = createStore(reducer, defaultState);
+
+store.dispatch({ type: "DECREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "INCREASE" });
+store.dispatch({ type: "RESET" });
+
 const App = () => {
-  return <Counter />;
+  return <Counter state={store.getState()} />;
 };
 
 export default App;
--- redux-counter/src/Counter.js
+++ redux-counter/src/Counter.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function Counter() {
+export default function Counter(props) {
+  const amount = props.state.count;
+  const name = props.state.name;
   const [count, setCount] = React.useState(0);
   return (
     <div className="container">
       <h1>counter</h1>
+      <h2>{name}</h2>
       <p className="counter">{count}</p>
       <div className="buttons">
         <button
