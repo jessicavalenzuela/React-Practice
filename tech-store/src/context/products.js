64cdc961d3687ca10ec6c024a0df26193fbce964
jessval <48558482+jessval@users.noreply.github.com>
2020-08-03 15:52:00 -0400

    Product context setup

--- tech-store/src/context/products.js
+++ tech-store/src/context/products.js
@@ -1,1 +1,15 @@
 // products context
+import React from "react";
+
+export const ProductContext = React.createContext();
+
+export default function ProductProvider({ children }) {
+  const greeting = "hi";
+  const product = { id: 1, title: "product name" };
+
+  return (
+    <ProductContext.Provider value={{ greeting, product }}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
--- tech-store/src/pages/Products.js
+++ tech-store/src/pages/Products.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { ProductContext } from "../context/products";
 
 export default function Products() {
+  const { greeting } = React.useContext(ProductContext);
   return <h1>hello from products page</h1>;
 }
